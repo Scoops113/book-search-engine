2b806030f0e553470cd7f6cf2201d6b64c6838be
Stephen <stephencooper11395@gmail.com>
2024-07-30 15:10:04 -0500

    edits

--- server/server.js
+++ server/server.js
@@ -34,6 +34,9 @@ const startServer = async () => {
 
     if (process.env.NODE_ENV === 'production') {
       app.use(express.static(path.join(__dirname, '../client/build')));
+      app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+      });
     }
 
     app.listen(PORT, () => {
