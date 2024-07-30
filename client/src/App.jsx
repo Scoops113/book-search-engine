5ce58a2acddaabb22ef77acb9fa1dd7f5eaa1717
Stephen <stephencooper11395@gmail.com>
2024-07-30 13:21:49 -0500

    edited app file to set up Apollo client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
-import './App.css';
+import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+import './App.css';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
 export default App;
+
